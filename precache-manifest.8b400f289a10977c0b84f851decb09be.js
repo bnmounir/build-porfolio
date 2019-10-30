6b372c6537746e7b1b4b926e3856a6a869b8ac5c
Mounir Benabderrahmane <bnmounir7@gmail.com>
2019-10-29 23:33:25 -0400

    in the works portfolio

---
+++ precache-manifest.8b400f289a10977c0b84f851decb09be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da55f59063f246edd50426a852fd299f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b533619d2758403981a",
+    "url": "/static/css/main.d34b287d.chunk.css"
+  },
+  {
+    "revision": "6b8c24b39078d3ab2e87",
+    "url": "/static/js/2.bbad4016.chunk.js"
+  },
+  {
+    "revision": "9b533619d2758403981a",
+    "url": "/static/js/main.a6eda358.chunk.js"
+  },
+  {
+    "revision": "a5be67a9b4bd7e979ca3",
+    "url": "/static/js/runtime-main.d9a55c3c.js"
+  }
+]);
